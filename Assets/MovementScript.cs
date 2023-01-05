a35318db3ff87355a976173925b8e8265c73eac7
ocampott <ocampott7@gmail.com>
2023-01-05 15:37:49 -0300

    animations and movement wip

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    public float speed = 1.0f;
+    public float Speed = 1.0f;
+    public float RotationSpeed = 1.0f;
+    public float JumpForce = 1.0f;
+    private Animator anim;
+    private Rigidbody Physics;
+    public float x, y;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Physics = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //Movimiento 
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(horizontal, 0.0f, vertical) * Time.deltaTime);
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        transform.Rotate(0, x * Time.deltaTime * RotationSpeed, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * Speed);
+
+        //Rotacion de la camara
+        float rotationY = Input.GetAxis("Mouse X");
+        transform.Rotate(new Vector3(0, rotationY * Time.deltaTime * RotationSpeed, 0));
+
+        //Salto
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Physics.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
+        }
+
+        //Animaciones
+        anim.SetFloat("Vel X", x);
+        anim.SetFloat("Vel Y", y);
+
     }
 }
